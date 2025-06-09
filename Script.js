a8595a11e7b365921b328de00a6d9875e1307f03
Thann Dy <dythann3@gmail.com>
2025-06-09 20:46:28 +0700

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,20 +1,20 @@
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesbnt = document.querySelector(".yes-btn");
-const nobnt = document.querySelector(".no-btn");
+// const question = document.querySelector(".question");
+// const gif = document.querySelector(".gif");
+// const yesbnt = document.querySelector(".yes-btn");
+// const nobnt = document.querySelector(".no-btn");
 
-yesbnt.addEventListener("click", () => {
-  question.innerHTML = "Right Now I Got You My Girlfiend 🩷";
-  gif.src = "photos.jpg";
-});
-nobnt.addEventListener("mouseover", () => {
-  const nobntRect = nobnt.getBoundingClientRect();
-  const maxX = window.innerWidth - nobntRect.Width;
-  const maxY = window.innerHeight - nobntRect.height;
+// yesbnt.addEventListener("click", () => {
+//   question.innerHTML = "Right Now I Got You My Girlfiend 🩷";
+//   gif.src = "photos.jpg";
+// });
+// nobnt.addEventListener("mouseover", () => {
+//   const nobntRect = nobnt.getBoundingClientRect();
+//   const maxX = window.innerWidth - nobntRect.Width;
+//   const maxY = window.innerHeight - nobntRect.height;
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+//   const randomX = Math.floor(Math.random() * maxX);
+//   const randomY = Math.floor(Math.random() * maxY);
 
-  nobnt.style.left = randomX + "px";
-  nobnt.style.top = randomY + "px";
-});
+//   nobnt.style.left = randomX + "px";
+//   nobnt.style.top = randomY + "px";
+// });
